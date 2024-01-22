b3b4e932c0d1c25fd8e7b79220509a247c0400d8
sasha <149568721+sasaki22877@users.noreply.github.com>
2024-01-22 11:05:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ youtube_video = 'https://www.youtube.com/watch?v=GV3Vb0cUsQY&list=PLBnDGoKqP7bbX
 youtube_inf1 = 'https://www.youtube.com/playlist?list=PLvtJKssE5Nrid_m5JTWqfAJuIDI4bNG_C'
 youtube_inf2 = 'https://www.youtube.com/watch?v=py20yTnkme4&list=PLudSw-n_9sI7E8bn6S1TBc1Bbg3U21uDK'
 youtube_inf3 = 'https://www.youtube.com/watch?v=A5chdV6tsSk&list=PLudSw-n_9sI58Gb9emvANIqM1RHIKocjr'
+youtube_ru1 = #вставить ссылку на видео с существительными
+youtube_ru2 = #глаголы
+youtube_ru3 = #прилагательные
